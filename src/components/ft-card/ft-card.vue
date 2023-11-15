523f84a3c51604dc7459fe77eb6994853f396d84
Zahid Rasool <86824661+Zahid-Rasool@users.noreply.github.com>
2023-11-15 11:19:46 +0500

    Create ft-card.vue

---
+++ src/components/ft-card/ft-card.vue
@@ -0,0 +1,11 @@
+<template>
+  <div
+    class="ft-card"
+    @focusout="$emit('focusout')"
+  >
+    <slot />
+  </div>
+</template>
+
+<script src="./ft-card.ts" />
+<style scoped lang="scss" src="./ft-card.scss" />
